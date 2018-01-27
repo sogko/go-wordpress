cea3b975dde600cc62c7a280ae59886ae123c6f1
Robbie Trencheny <me@robbiet.us>
2018-01-26 16:46:13 -0800

    Support time with zone info for Users.RegisteredDate

--- time.go
+++ time.go
@@ -11,12 +11,24 @@ type Time struct {
 // 2017-12-25T09:54:42
 const timeLayout = "2006-01-02T15:04:05"
 
-func (t *Time) UnmarshalJSON(b []byte) (err error) {
+// "2017-09-24T13:28:06+00:00"
+const timeWithZoneLayout = "2006-01-02T15:04:05-07:00"
+
+func (t *Time) UnmarshalJSON(b []byte) error {
 	if b[0] == '"' && b[len(b)-1] == '"' {
 		b = b[1 : len(b)-1]
 	}
-	t.Time, err = time.Parse(timeLayout, string(b))
-	return
+	tTime, err := time.Parse(timeLayout, string(b))
+	if err != nil {
+		altTime, altErr := time.Parse(timeWithZoneLayout, string(b))
+		if altErr != nil {
+			return err
+		} else {
+			t.Time = altTime
+		}
+	}
+	t.Time = tTime
+	return nil
 }
 
 func (t *Time) MarshalJSON() ([]byte, error) {
